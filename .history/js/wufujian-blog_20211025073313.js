a671297375f249fa34d18651e08c09e0eb4c1636
txwfj <46244590+txwfj@users.noreply.github.com>
2022-01-27 10:44:23 +0800

    ss

--- .history/js/wufujian-blog_20211025073313.js
+++ .history/js/wufujian-blog_20211025073313.js
@@ -4,10 +4,14 @@
  * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
  */
 
-
+ /*!
+ * Hux Blog v1.6.0 (http://startbootstrap.com)
+ * Copyright 2016 @huxpro
+ * Licensed under Apache 2.0 
+ */
 
 // Tooltip Init
-// Unuse by Wufujian since V1.6: Titles now display by default so there is no need for tooltip
+// Unuse by Hux since V1.6: Titles now display by default so there is no need for tooltip
 // $(function() {
 //     $("[data-toggle='tooltip']").tooltip();
 // });
@@ -15,7 +19,7 @@
 
 // make all images responsive
 /* 
- * Unuse by Wufujian
+ * Unuse by Hux
  * actually only Portfolio-Pages can't use it and only post-img need it.
  * so I modify the _layout/post and CSS to make post-img responsive!
  */
